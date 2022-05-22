e7d03ce6918caf0ec59ddc048d9e61a397524cd5
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-05-22 14:26:58 +0200

    Validates read_lf() and add options to regulate process output

--- R/import.R
+++ R/import.R
@@ -1,4 +1,5 @@
-#' @importFrom dplyr     bind_rows
+#' @importFrom dplyr     %>% bind_rows filter
 #' @importFrom readr     col_character col_date col_datetime col_double
-#'                       col_integer cols read_csv
+#'                       col_integer cols problems read_csv
+#' @importFrom rlang    .data
 NULL
