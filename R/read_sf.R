b685e71d98a4e5764e15c7d78214d7b063e5b34b
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-05-22 22:16:02 +0200

    Stop babbles by read_csv() by arguments verbose, progress and warnings.

--- R/read_sf.R
+++ R/read_sf.R
@@ -1,29 +1,30 @@
 #' Read sf data
 #'
-#' @param type Character, either "fixed" or "adaptive"
-#' @param onedrive Character, the location of the local OneDrive sync
-#' @param path Character, path name within the OneDrive
+#' @inheritParams read_lf
 #' @return A tibble with the original data and one new column named `file`
 #' containing the original file name. If `type == "fixed"` a tibble with 159
 #' columns, with one test administration per row. If `type == "adaptive"`, a tibble
 #' with 14 columns, with one item administration per row.
 #' @export
-read_sf <- function(
-  type = c("fixed", "adaptive"),
-  onedrive = Sys.getenv("ONEDRIVE_GSED"),
-  path = "GSED Final Collated Phase 1 Data Files 18_05_22") {
-
-  stopifnot(nchar(onedrive) > 0L)
+read_sf <- function(type = c("fixed", "adaptive"),
+                    onedrive = Sys.getenv("ONEDRIVE_GSED"),
+                    path = "GSED Final Collated Phase 1 Data Files 18_05_22",
+                    verbose = FALSE,
+                    progress = FALSE,
+                    warnings = FALSE) {
+  if (nchar(onedrive) == 0L) {
+    stop("Environmental variable ONEDRIVE_GSED not set.", call. = FALSE)
+  }
   type <- match.arg(type)
 
   if (type == "fixed") {
-    return(read_sf_fixed(onedrive, path))
+    return(read_sf_fixed(onedrive, path, verbose, progress, warnings))
   } else {
-    return(read_sf_adaptive(onedrive, path))
+    return(read_sf_adaptive(onedrive, path, verbose, progress, warnings))
   }
 }
 
-read_sf_fixed <- function(onedrive, path) {
+read_sf_fixed <- function(onedrive, path, verbose, progress, warnings) {
   # hardcode fixed sf files names
   files_fixed <- c(
     "tan/tza-sf-2021-11-01.csv",
@@ -38,25 +39,21 @@ read_sf_fixed <- function(onedrive, path) {
 
   # read
   files <- file.path(onedrive, path, files_fixed)
-  data <- vector(mode = "list", length = length(files))
-  nm <- tolower(gsub("-", "_", basename(files)))
-  names(data) <- unlist(lapply(strsplit(nm, split = "\\."), `[`, 1L))
   date_formats <- c("%Y-%m-%d", "%d-%m-%Y", "%d-%m-%Y",
                     "%d/%m/%Y", "%d/%m/%Y", "%d/%m/%Y",
                     "%m/%d/%Y", "%m/%d/%Y", "%m/%d/%Y")
-  for (i in 1:length(files)) {
-    spec <- define_col("sf", "fixed", date_formats[i])
-    data[[i]] <- readr::read_csv(files[i],
-                                 col_types = spec,
-                                 na = c("", "NA", "-8888", "-8,888.00", "-9999"),
-                                 show_col_types = FALSE)
-  }
+  data <- read_files("sf", "fixed", files, 1:length(files),
+                     date_formats, NULL,
+                     verbose, progress, warnings)
 
   # bind
-  return(dplyr::bind_rows(data, .id = "file"))
+  # remove orphan records without a GSED_ID
+  data %>%
+    bind_rows(.id = "file") %>%
+    filter(!is.na(.data$GSED_ID))
 }
 
-read_sf_adaptive <- function(onedrive, path) {
+read_sf_adaptive <- function(onedrive, path, verbose, progress, warnings) {
 
   # hardcode adaptive sf files names
   files_adaptive <- c(
@@ -65,40 +62,35 @@ read_sf_adaptive <- function(onedrive, path) {
     "ban/ban_sf_adaptive_17_05_2022.csv",
     "ban/ban_sf_new_adaptive_17_05_2022.csv",
     "pak/pak_sf_adaptive_2022_05_17.csv",
-    "pak/pak_sf_new_adaptive_2022_05_17.csv")
+    "pak/pak_sf_new_adaptive_2022_05_17.csv"
+    )
   files <- file.path(onedrive, path, files_adaptive)
-  data <- vector(mode = "list", length = length(files))
-  nm <- tolower(gsub("-", "_", basename(files)))
-  names(data) <- unlist(lapply(strsplit(nm, split = "\\."), `[`, 1L))
   date_formats <- c("%d-%m-%Y", "%d-%m-%Y",
                     "%d-%m-%Y", "%d/%m/%Y",
                     "%m/%d/%Y", "%m/%d/%Y")
   datetime_formats <- c("%d-%m-%Y %H:%M:%S", "%d-%m-%Y %H:%M",
                         "%d-%m-%Y %H:%M:%S", "%d/%m/%Y %H:%M",
                         "%d-%m-%Y %H:%M:%S", "%d-%m-%Y %H:%M:%S")
-  for (i in 1:length(files)) {
-    spec <- define_col("sf", "adaptive", date_formats[i],
-                       datetime_format = datetime_formats[i])
-    data[[i]] <- readr::read_csv(files[i],
-                                 col_types = spec,
-                                 na = c("", "NA", "-8888", "-8,888.00", "-9999"),
-                                 show_col_types = FALSE)
-  }
+  data <- read_files("sf", "adaptive", files, 1:length(files),
+                     date_formats, datetime_formats,
+                     verbose, progress, warnings)
 
   # repair mixed time stamp format in pak_sf_adaptive_2022_05_17 and
   # pak_sf_new_adaptive_2022_05_17 (about 15% of the values).
-  for (i in 5:6) {
-    spec <- define_col("sf", "adaptive", date_formats[i],
-                       datetime_format = "%m/%d/%Y %H:%M")
-    tmp <- readr::read_csv(files[i],
-                           col_types = spec,
-                           na = c("", "NA", "-8888", "-8,888.00", "-9999"),
-                           show_col_types = FALSE)
+  idx <- 5:6
+  datetime_formats <- rep("%m/%d/%Y %H:%M", 6)
+  tmp <- read_files("sf", "adaptive", files, idx,
+                    date_formats, datetime_formats,
+                    verbose, progress, warnings)
+  for (i in idx) {
     z <- data[[i]]$Ma_SF_timestamp
-    z[is.na(z)] <- tmp$Ma_SF_timestamp[!is.na(tmp$Ma_SF_timestamp)]
+    z[is.na(z)] <- tmp[[i]]$Ma_SF_timestamp[!is.na(tmp[[i]]$Ma_SF_timestamp)]
     data[[i]]$Ma_SF_timestamp <- z
   }
 
   # bind
-  return(dplyr::bind_rows(data, .id = "file"))
+  # remove orphan records without a GSED_ID
+  data %>%
+    bind_rows(.id = "file") %>%
+    filter(!is.na(.data$GSED_ID))
 }
