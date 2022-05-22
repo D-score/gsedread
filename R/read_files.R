a1f91804e1afca94aa942853b60014e65b91f7a4
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-05-22 20:36:19 +0200

    Move the file reading work to a function read_files()

---
+++ R/read_files.R
@@ -0,0 +1,35 @@
+read_files <- function(test, type, files, idx,
+                       date_formats, datetime_formats,
+                       verbose, progress, warnings) {
+  stopifnot(length(files) == length(date_formats))
+  if (is.null(datetime_formats)) datetime_formats <- rep("", length(files))
+  stopifnot(length(files) == length(datetime_formats))
+
+  data <- vector(mode = "list", length = length(files))
+  nm <- tolower(gsub("-", "_", basename(files)))
+  names(data) <- unlist(lapply(strsplit(nm, split = "\\."), `[`, 1L))
+
+  for (i in idx) {
+    fn <- files[i]
+    if (!file.exists(fn)) stop("File not found:", fn)
+    spec <- define_col(test, type,
+                       date_formats[i],
+                       datetime_format = datetime_formats[i])
+    if (!warnings) {
+      suppressWarnings(
+        data[[i]] <- readr::read_csv(fn,
+                                     col_types = spec,
+                                     na = c("", "NA", "-8888", "-8,888.00", "-9999"),
+                                     show_col_types = verbose,
+                                     progress = progress)
+      )
+    } else {
+      data[[i]] <- readr::read_csv(fn,
+                                   col_types = spec,
+                                   na = c("", "NA", "-8888", "-8,888.00", "-9999"),
+                                   show_col_types = verbose,
+                                   progress = progress)
+    }
+  }
+  data
+}
